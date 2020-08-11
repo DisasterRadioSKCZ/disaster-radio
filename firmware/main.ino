286af20f427fca955ac8f82d18cc11fedb2bfadd
tomas <tomas.olah@gmail.com>
2020-08-11 15:36:34 +0200

    pridana volba pre konfiguraciu spreading factoru

--- firmware/main.ino
+++ firmware/main.ino
@@ -343,6 +343,8 @@ void setupLoRa()
   Layer1Class *Layer1_1 = new Layer1Class();
   Layer1->setPins(LORA_CS, LORA_RST, LORA_IRQ);
   Layer1->setLoRaFrequency(LORA_FREQ);
+  Layer1.setSpreadingFactor(LORA_SPREAD);
+
   #endif
   #ifdef RL_SX1276
   pinMode(LORA_CS, OUTPUT);
